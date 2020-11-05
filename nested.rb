fe8cf405d0b4aa491b411b0451ffd944ba75cef1
gentle-expression-4880 <choim3@spu.edu>
2020-11-05 20:09:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-    return programmer_hash[:alan_kay][:known_for] = "GUI"
+   programmer_hash[:alan_kay][:known_for] = "GUI"
+   return programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
